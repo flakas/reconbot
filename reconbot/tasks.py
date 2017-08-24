5466e0c655873a85fe1633629c1b398cd1b197cc
Tautvidas Sipavičius <t.9b5as@gmail.com>
2017-08-24 16:05:08 +0300

    Fix discord notification task

--- reconbot/tasks.py
+++ reconbot/tasks.py
@@ -6,7 +6,7 @@ import evelink.eve
 
 from reconbot.eve import Eve
 from reconbot.notificationprinters.slack import Slack
-from reconbot.notificationprinters.slack import Discord
+from reconbot.notificationprinters.discord import Discord
 
 def notification_task(db, notification_options, api_queue, printer, notifier):
     MAX_NOTIFICATION_AGE_IN_SECONDS = 3600
