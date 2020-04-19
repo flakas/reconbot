c517b3423916508f820167c9cede2804b2cd1b8f
soneill <sam@adcurve.com>
2020-04-19 22:24:12 +0200

    updates ESI time to 45 minutes

--- reconbot/tasks.py
+++ reconbot/tasks.py
@@ -5,7 +5,7 @@ from reconbot.notificationprinters.esi.discord import Discord as ESIDiscord
 from reconbot.esi import ESI
 
 def esi_notification_task(notification_options, api_queue, printer, notifier):
-    MAX_NOTIFICATION_AGE_IN_SECONDS = 86400*7
+    MAX_NOTIFICATION_AGE_IN_SECONDS = 2700
 
     try:
         sso = api_queue.get()
